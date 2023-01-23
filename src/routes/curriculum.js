88b8a93764383b1c7978bc800c493f303c219e18
ezkemboi <ezrqnkemboi@gmail.com>
2023-01-23 08:16:54 +0300

    more set up on logos

--- src/routes/curriculum.js
+++ src/routes/curriculum.js
@@ -1,5 +1,32 @@
 // import { Link } from 'react-router-dom'
 
+
+const data = [
+  {
+    week1: {
+      "Computer science concepts": [],
+      "Role of computers in today’s world": [],
+      "Computer architecture": [],
+      "E-commerce": [],
+      "Computer-related jobs": []
+    },
+    week2: {
+      "Introduction to programming": [],
+      "Introduction to web design and development": [],
+      "Introduction to mobile development": []
+    },
+    week3: {
+      "Version control": [],
+      "Introduction to GitHub": [],
+      "Introduction to databases": []
+    },
+    week4: {
+      "Computer security": [],
+      "Introduction to desktop application": [],
+    }
+  }
+]
+
 const Curriculum = () => {
   return (
     <div className="App">
